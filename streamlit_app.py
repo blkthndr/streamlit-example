080453a1820269275642f46b0eb210bfd1629ba4
blkthndr <69771487+blkthndr@users.noreply.github.com>
2024-02-01 00:13:57 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,19 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+
+from propelauth import auth
+
+user = auth.get_user()
+if user is None:
+    st.error('Unauthorized')
+    st.stop()
+
+with st.sidebar:
+    st.link_button('Account', auth.get_account_url(), use_container_width=True)
+
+st.text("Logged in as " + user.email + " with user ID " + user.user_id)
+
 """
 # Welcome to Streamlit! Boom
 
